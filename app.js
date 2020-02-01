dc0f3e70a6ce530c900e5504206bb82926d9a9fa
Janos Orban <janos.orban7@gmail.com>
2020-02-01 16:08:37 +0100

    Update app.js

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ if (process.env.PORT) {
 
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Welcome to mod IBM Cloud DevOps using containers. Let\'s go use the Continuous Delivery Service');
-});
+  res.send('Welcome to IBM Cloud DevOps using containers. Let\'s go use the Continuous Delivery Service');
+})
 
 app.listen(PORT);
 console.log(' Application Running on port' + PORT);
